4b4e9275e2bce17aec7fd8dec15ef94b0701ca25
slugwurth <29309384+slugwurth@users.noreply.github.com>
2020-10-17 18:35:08 -0400

    Update timeLapse.m
    added: prepVideo(), compileVideo(), cropImage(), matchCrop()
    
    todo: more video parameter mod access before build

--- timeLapse.m
+++ timeLapse.m
@@ -1,4 +1,4 @@
-classdef  timeLapse
+classdef  timeLapse < handle
     % Time Lapse Writer
     % Matt Ireland 2020
     
@@ -12,6 +12,12 @@ classdef  timeLapse
         name
         imageNames
         frate
+        
+        cropped
+        currentWindow
+        
+        outputVideo
+        
     end
     
     % Class Constructor
@@ -25,30 +31,105 @@ classdef  timeLapse
             iNames = dir(fullfile(pwd,'*.jpg'));
             obj.imageNames = {iNames.name}';
             
-            % Compile video
-            obj = compileVideo(obj);
+            % Write out options
+            y = {
+                ''
+                'Time Lapse Object Created'
+                'Options:'
+                ' '
+                '     obj.prepVideo(obj,varargin) '
+                '       % make the video file at framerate=varargin, or if absent: at framerate=obj.frate'
+                '     obj.compileVideo(framerate) '
+                '       % compiles the video in pwd'
+                '     obj.cropImage(obj,index,window)'
+                '       % crops the indexed image by window=[xmin ymin dx dy] (4th quadrant coordsys)'
+                '     obj.matchCrop(obj,index,rFlag)'
+                '       % matches the previously cropped window to indexed image, renderFlag=[0,1] '
+                };
+            fprintf('%s\n',y{:});
         end
     end
     
     % Future capabilities
-    methods 
+    methods
+        
+        function obj = prepVideo(obj,varargin)
+            
+            if ~isempty(nargin); obj.frate = varargin; end
+            
+            % Initialize output file
+            obj.outputVideo = VideoWriter(fullfile(pwd,obj.name));
+            obj.outputVideo.FrameRate = obj.frate;
+        end
         
         function obj = compileVideo(obj)
-            % Initialize output file
-            outputVideo = VideoWriter(fullfile(pwd,obj.name));
-            outputVideo.FrameRate = obj.frate;
-            open(outputVideo)
+            
+            % open the prepped file
+            open(obj.outputVideo)
             
             % Add files sequentially
             for ii = 1:length(obj.imageNames)
                 disp(['Frame ' num2str(ii) ' of ' num2str(length(obj.imageNames))]);
                 img = imread(fullfile(pwd,obj.imageNames{ii}));
-                writeVideo(outputVideo,img)
+                writeVideo(obj.outputVideo,img)
             end
             
             % close the file
-            close(outputVideo);
-
+            close(obj.outputVideo);
+        end
+        
+        function obj = cropImage(obj,index,window)
+            % Accepts:
+            %       -obj of the class
+            %       -image index
+            %       -cropping window
+            % Sets the current window property
+            % Populates obj.cropped
+            
+            obj.currentWindow = window;
+            img = imread(char(obj.imageNames(index)));
+            
+            obj.cropped = imcrop(img,window);
+            
+        end
+        
+        function obj = matchCrop(obj,index,renderFlag)
+            
+            % assign variables
+            crop = obj.cropped;
+            fullFrame = imread(char(obj.imageNames(index)));
+            % normalized cross-correllation
+            c = normxcorr2(crop(:,:,1),fullFrame(:,:,1));
+            % offset found by correlation
+            [~, imax] = max(abs(c(:)));
+            [ypeak, xpeak] = ind2sub(size(c),imax(1));
+            offset = [(xpeak-size(crop,2)) (ypeak-size(crop,1))];
+            xoffset = offset(1); yoffset = offset(2);
+            xbegin = round(xoffset+1);
+            xend   = round(xoffset+ size(crop,2));
+            ybegin = round(yoffset+1);
+            yend   = round(yoffset+size(crop,1));
+            % extract region from fullFrame and compare to crop
+            extracted_crop = fullFrame(ybegin:yend,xbegin:xend,:);
+            if isequal(crop,extracted_crop)
+                disp('The crop is from the same image')
+            end
+            % place the crop in an empty image sized equal to fullFrame
+            recovered_crop = uint8(zeros(size(fullFrame)));
+            recovered_crop(ybegin:yend,xbegin:xend,:) = crop;
+            
+            % show crop overlay data
+            if renderFlag == 1
+                figure();
+                contourf(c); 
+                pbaspect([size(fullFrame,[1 2])'; 1]')
+                
+                figure(); 
+                imshow(recovered_crop);
+                
+                figure(); 
+                imshowpair(fullFrame(:,:,1),recovered_crop,'blend');
+            end
         end
     end
 end
